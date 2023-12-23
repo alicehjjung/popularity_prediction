cf6c0078831c39e66f784a8f1db8c2f3e775dc46
HYOJUNG08 <101383752+HYOJUNG08@users.noreply.github.com>
2023-12-23 18:12:49 +0900

    Update preprocessing function

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -9,6 +9,7 @@ from category_encoders import OneHotEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.compose import ColumnTransformer
 import pandas as pd
+from pathlib import Path
 from sklearn.preprocessing import StandardScaler
 
 def loading():
@@ -18,22 +19,13 @@ def loading():
         falling_speed=5,
         animation_length="infinite",
     )
-
 def preprocessing(data):
     #data['mode'] = data['mode'].astype(str)
     #data['explicit'] = data['explicit'].astype(str)
 
     scaler = StandardScaler()
-    data[['duration_ms','danceability','energy','loudness','acousticness','valence','tempo']] = \
-        scaler.fit_transform(data[['duration_ms','danceability','energy','loudness','acousticness','valence','tempo']])
-    '''
-    if data.loc[0, 'mode']==0:
-        data.loc[0,'mode_0']=1
-        data.loc[0,'mode_1']=0
-    else:
-        data.loc[0,'mode_0']=0
-        data.loc[0,'mode_1']=1
-    '''
+    data[['duration_ms','danceability','energy','loudness','acousticness','valence','tempo']] = scaler.fit_transform(data[['duration_ms','danceability','energy','loudness','acousticness','valence','tempo']])
+    
     if data.loc[0,'explicit']==False:
         data.loc[0, 'Category_False']=1
         data.loc[0, 'Category_True']=0
@@ -45,6 +37,7 @@ def preprocessing(data):
     #encoder = OneHotEncoder(use_cat_names = True)
     #data = encoder.fit_transform(data)
     data = data.reindex(sorted(data.columns), axis=1)
+    # data = data.drop(columns=['duration_ms'])
     #print(data)
     return data
 
@@ -55,12 +48,17 @@ def scaling(data):
     return data
 
 def get_predictions(data):
-    model_path = ''
+    model_path = Path(__file__).parent
+    model_path = model_path/'final_model.pkl'
     model = joblib.load(model_path)
     predictions = model.predict(data)
     return predictions
 
 def main():
+    if st.button("🌊 Rerun"):
+        st.rerun()
+
+    
     st.title("Predict Your Song")
 
     artist_name = st.text_input("🎤 Artist")
@@ -68,32 +66,37 @@ def main():
         
     if st.button("Popularity Prediction"):
         #난수 생성
-        random_value = random.choice([0, 1])
-        if random_value >= 0.5:
-            '''
-            #객체 생성
-            s = Spotify(artist_name,song_title)
-            #변수값 출력
-            print(s.getTrackInfo())
-            print(s.artist)
-            print(s.track)  
-            '''
-
-            with st.spinner('Wait for it...'):
-                #예측값 받아오기
-                #data = [[230666,False,0.676,0.461,1,-6.746,0,0.143,0.0322,1.01e-06,0.358,0.715,87.917,4,0]]
-                data={'acousticness':0.0322,'danceability':0.676,'duration_mins':3.8444333333333334,'energy':0.461,'explicit':False,'instrumentalness':1.01e-06,'loudness':-6.746,'mode':0,'popularity':73,'tempo':87.917,'valence':0.715}
-                data = pd.DataFrame([data])
+        #객체 생성
+        s = Spotify(artist_name,song_title,"1")
+        # print(s.artist)
+        # print(s.track)  
+        print(s.img)
+        with st.spinner('Wait for it...'):
+            #예측값 받아오기
+            try:
+                data = s.getTrackInfo()
                 data = preprocessing(data)
-                #data = scaling(data)
-                #data=[[0.645,0.537,5.51555,0.342,1,0,0.266,-13.553,0,1,109.236,0.253]]
+                # print("data : ")
+                # for i in data:
+                #     print(i)
                 pred = get_predictions(data)
-                #print(pred)
-                time.sleep(3)
-            st.success(f"Prediction for Artist: {artist_name}, Track Name: {song_title}, Prediction: {pred}")
-        else:
-            loading()
-            st.error("Oops! Something went wrong.")
+                print("pred : ",pred)
+                try:
+                    st.image(s.img)
+                except:
+                    st.warning("Sorry, no image available")
 
+                if(1):
+                    if pred == 1:
+                        st.success('💻 Predicted Popularity Level : 1 (1/1, popular!)')                
+                    else :
+                        st.success('📺 Predicted Popularity Level : 0 (0/1, unpopular)')
+                    #data = scaling(data)
+                    #print(pred)
+                        time.sleep(3)
+            except:
+                st.warning("Sorry, please try with another song")
+        
+        st.cache_data.clear()        
 if __name__ == "__main__":
     main()
